9297fe8d57cdc2570b64e411dcafc8c47a36d28f
Ian Yanik <46629480+IanYanik@users.noreply.github.com>
2019-09-16 08:01:13 -0400

    added looper, looper2, count2, and added for statement to outline.

---
+++ JAVA programs/Count2.java
@@ -0,0 +1,8 @@
+public class Count2 {
+    public static void main(String[] args) {
+        for (int i = i<=3; i++;) 
+            System.out.println("How many lines");
+ System.out.println("are printed?");
+        
+    }
+}
---
+++ JAVA programs/Looper.java
@@ -0,0 +1,11 @@
+public class Looper {
+    public static void main(String[] args) {
+ for (int i = i<=10; i++;){
+ for (int i = i<=10; i++;){       
+ for (int i = i<=10; i++;){
+            System.out.println(i);
+            }
+            }
+            }
+    }
+}
