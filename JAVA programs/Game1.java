12992676cb0a729f07df42e369128b84d3bb40b8
Ian Yanik <46629480+IanYanik@users.noreply.github.com>
2019-09-18 07:59:19 -0400

    Made attempt at a game

---
+++ JAVA programs/Game1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Game1
+{
+    public static void main(String args[]) 
+    {
+      Scanner keyboard = new Scanner(System.in);
+System.out.println("Attack or heal");
+int attackOrHeal = keyboard.nextInt();
+      System.out.println(myint);
+    }
+}
