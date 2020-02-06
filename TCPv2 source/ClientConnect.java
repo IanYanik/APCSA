1b8ac8f793edcf5e87cc7921bba406413c886450
Ian Yanik <46629480+IanYanik@users.noreply.github.com>
2020-02-06 08:08:31 -0500

    r

--- TCPv2 source/ClientConnect.java
+++ TCPv2 source/ClientConnect.java
@@ -2,10 +2,31 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 public class ClientConnect {
-public static void connect(Socket socket){
-OutputStream output = socket.getOutputStream();
-PrintWriter writer = new PrintWriter(output, true);
-Console console = System.console();
-
+OutputStream output;
+PrintWriter writer;
+Console console;
+Socket socket;
+public void connect(Socket socket2){
+socket = socket2;
+try{
+output = socket.getOutputStream();
+writer = new PrintWriter(output, true);
+console = System.console();
+} catch (UnknownHostException ex) {
+System.out.println("Server not found: " + ex.getMessage());
+} catch (IOException ex) { 
+System.out.println("I/O error: " + ex.getMessage());
+}
+}
+public void sendMessage(String message){
+try{
+if(message.equals("bye")){
+socket.close();
+}
+}
+catch(Exception e) {
+System.out.println("Oh no");
+}
+writer.println(message);
 }
 }
